012dc70b0fa74cede308601d3fb4e2d29e4f00eb
Michael Baumann <michi.don@gmail.com>
2015-01-11 21:19:36 +0100

    Designed UI + enable Drag-and-Drop
    First design of the UI. Also enabled the application to accept files via
    drag-and-drop and display the filenames in the list.

---
+++ BatchFileRename.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-01-11T19:51:49
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = BatchFileRename
+TEMPLATE = app
+CONFIG += c++11
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,33 @@
+#include <QtCore/QMimeData>
+#include <QtGui/QDropEvent>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setAcceptDrops(true);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent *event) {
+    event->acceptProposedAction();
+}
+
+void MainWindow::dropEvent(QDropEvent *event) {
+    // Source: https://stackoverflow.com/questions/14895302
+    for (auto url: event->mimeData()->urls()) {
+        QString filePath = url.toLocalFile();
+        QString shownText = QString("%1 -> (%2)")
+                .arg(filePath.split("/").last())
+                .arg(filePath);
+        ui->listWidget->addItem(shownText);
+    }
+}
